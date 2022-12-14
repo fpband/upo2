7a374ab33615ce9bc1aedd0b773d6b3e60b488a7
fpband <82159763+fpband@users.noreply.github.com>
2022-12-14 11:13:59 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/sahaynitin4tellyfun/Uploader-Bot-V2 /Uploader-Bot-V2
+git clone https://github.com/fpband/upo2 /upo2
 cd /Uploader-Bot-V2
 pip3 install -U -r requirements.txt
 echo "Starting Bot.... Please Wait. Check You Log. Bot Credit @DKBOTZ\n Special Credit :- @Tellybots"
